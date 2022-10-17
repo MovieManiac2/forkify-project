91d9d69e141e8fc11dd3e18a6ea06aba5b76f45b
MovieManiac2 <ryluz03@hotmail.com>
2022-10-17 12:08:57 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b0f7ae14-49a7-4baa-b863-98b940b941e1';
+export const MODAL_CLOSE_SEC = 2.5;
