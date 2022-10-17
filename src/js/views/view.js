6047fd720999b06e6e0110d8932a6b1fb8b202a3
MovieManiac2 <ryluz03@hotmail.com>
2022-10-17 13:32:53 -0300

    Fourth try

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import { Fraction } from '../../../node_modules/fractional/array_mixins';
 
 export default class View {
   _data;
